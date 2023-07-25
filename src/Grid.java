ae10a01f9d65fcdac99082d3b970484e41eae773
Janet Suryadi <janet@suryadi.id.au>
2023-07-25 23:46:30 +1000

    Adding in constructor

--- src/Grid.java
+++ src/Grid.java
@@ -8,6 +8,7 @@ public class Grid {
     // For keeping track of how many bombs are adjacent to a cell to render in the cell's place 
     private int bombTally = 0;
 
+    private String[][] grid;
 
     
     /**--------------------------------------------
@@ -17,6 +18,14 @@ public class Grid {
     Grid(int rows, int cols) {
         setRows(rows);
         setCols(cols);
+        grid = new String[rows][cols];
+
+        // Setting all the initial values of the grid to ■
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = "\u25A0";
+            }
+        }
     }
 
     /**
@@ -41,6 +50,18 @@ public class Grid {
     public static void setCols(int cols) {
         if (cols > 0) {
             Grid.cols = cols;
+        }
+    }
+
+    /**--------------------------------------------
+     *               METHODS
+     *---------------------------------------------**/
+    public void renderGrid(int[][] gridToRender) {
+        for (int i = 0; i < gridToRender.length; i++) {
+            for (int j = 0; j < gridToRender[i].length; j++) {
+                System.out.print(gridToRender[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
