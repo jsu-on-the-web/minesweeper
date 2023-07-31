77172b773c65bd1611a5af2ef45e7d67e09f6f6f
Janet Suryadi <janet@suryadi.id.au>
2023-07-31 10:25:28 +1000

    Further Game Logic Programming

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -10,7 +10,6 @@ public class GameLogic {
     private boolean gameEnded = false;
     private boolean playerWon;
 
-
     /**
      * ------------------------------------------------------------------------
      * METHODS
@@ -33,16 +32,26 @@ public class GameLogic {
      **/
 
     public void playGame() {
+        // Setting up input
         Scanner scanner = new Scanner(System.in);
         while (!gameEnded) {
+            // We should render the grid on every turn
             grid.renderGrid(grid);
-            //grid.renderBombLocations(grid);
+            // grid.renderBombLocations(grid);
 
+            // Grabbing input and trying to parse it as a
             System.out.println("Enter the row and column of the box you want to reveal.");
             int row = scanner.nextInt();
             int col = scanner.nextInt();
 
             pickBox(row, col);
+
+            // * Check for win and lose conditions
+
+            if (checkIfAllBoxesChecked()) {
+                gameEnded = true;
+                playerWon = true;
+            }
         }
     }
 
@@ -50,7 +59,15 @@ public class GameLogic {
         return false;
     }
 
+    /**
+     * Checks if a box can be picked.
+     *
+     * @param  row   the row index of the box
+     * @param  col   the column index of the box
+     * @return       true if the box can be picked, false otherwise
+     */
     public boolean pickBox(int row, int col) {
+        // * Guard clauses
         if (grid.getBox(row, col) == null) {
             System.out.println("This box is invalid.");
             return false;
@@ -61,19 +78,30 @@ public class GameLogic {
             return false;
         }
 
-        // Let's see if there's a bomb
+        // * Valid box! Time to check its value.
+        return checkBox(row, col);
+    }
+
+    /**
+     * Checks if there's a bomb at the specified row and column.
+     *
+     * @param  row  the row index of the box
+     * @param  col  the column index of the box
+     * @return      true regardless of whether there's a bomb or not
+     */
+    public boolean checkBox(int row, int col) {
+        // * Let's see if there's a bomb
         if (grid.getBox(row, col).isRevealed() != false && grid.getBox(row, col).isHasBomb()) {
             grid.getBox(row, col).setCurrentIcon("X");
-            playerWon = false; 
+            playerWon = false;
             gameEnded = true;
             return true;
+        } else {
+            // * Else it must be a regular box
+            grid.getBox(row, col).setRevealed(true);
+            grid.checkForAdjacentBomb(row, col); // ? Try implementing a cascading function?
+            grid.getBox(row, col).currentIcon = grid.getBombTally() + "";
+            return true;
         }
-
-        // Else it must be a regular box
-        grid.getBox(row, col).setRevealed(true);
-        grid.checkForAdjacentBomb(row, col); // ? Try implementing a cascading function?
-        grid.getBox(row, col).currentIcon = grid.getBombTally() + "";
-        return true; 
-
     }
 }
